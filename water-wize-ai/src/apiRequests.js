dc1e1482ead12962e959d1c86fcfba7acbb75d16
HadarSabati <hadarsabati@gmail.com>
2024-06-27 13:30:57 +0300

    comment

--- water-wize-ai/src/apiRequests.js
+++ water-wize-ai/src/apiRequests.js
@@ -186,7 +186,7 @@ export const getAllUsers = async (userId) => {
 export const addNewForumMessage = async (message) => {
   try {
     //const recommendations = message.recommendation.map(id => ObjectId(id));
-    console.log("fffffffffff" + message.recommendations)
+    //console.log("fffffffffff" + message.recommendations)
     const response = await fetch('/forum/newMessage', {
       method: "POST",
       headers: {
@@ -245,7 +245,7 @@ export const addNewForumComment = async (comment) => {
 
 export const getAllCommentsForMsg = async (msgId) => {
   try {
-    console.log(msgId)
+    //console.log(msgId)
     const response = await fetch(`/forum/messages/${msgId}/comments`, {
       method: "GET",
       headers: {
@@ -253,7 +253,7 @@ export const getAllCommentsForMsg = async (msgId) => {
       },
     });
     const comments = await response.json();
-    console.log(comments)
+    //console.log(comments)
     return comments
   } catch (error) {
     console.error("Error:", error);
