5d749150a06ab6bcd2c112477c558e0e225696fd
HadarSabati <hadarsabati@gmail.com>
2024-06-27 11:05:02 +0300

    home page

--- water-wize-ai/src/pages/Home.js
+++ water-wize-ai/src/pages/Home.js
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 import RegisterForm from "../components/RegisterForm";
 // import classes from "../styles/Register_and_Login.css";
 // import classes from "../styles/Home.module.css";
-import { Box } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 import irrigationImg from '../resources/images/irrigation.jpg';
 import ContainerBox from "../components/ContainerBox";
 import WaterCalculator from "./WaterCalculator";
@@ -25,7 +25,7 @@ const containerStyle = {
 
 const containerStyleLeft = {
   position: 'absolute',
-  bottom: '-700px', // Adjust as needed
+  bottom: '-1px', // Adjust as needed
   left: '10px', // Adjust as needed
   zIndex: 10, // Ensures the div is in the front
   width: '500px', // Adjust as needed
@@ -38,64 +38,52 @@ const containerStyleLeft = {
 function Home() {
   return (
     <PageContainer
-    children={
-      <Box 
-      sx={{
-      //   backgroundImage: `url(${imsLogoImg})`,
-      //   backgroundSize: 'cover',
-        // backgroundPosition: 'center',
-        height: '70vh',
-        width: '100vw',
-        display: 'flex',
-        // alignItems: 'center',
-        justifyContent: 'center',
-        border:"var(--medium-green)"
-      //   marginTop:"-88px",
-      //   marginBottom:"-88px"
-       }}
-  >
+      children={
+        <Box
+          sx={{
+            //   backgroundImage: `url(${imsLogoImg})`,
+            //   backgroundSize: 'cover',
+            // backgroundPosition: 'center',
+            height: '70vh',
+            width: '100vw',
+            display: 'flex',
+            // alignItems: 'center',
+            justifyContent: 'center',
+            border: "var(--medium-green)"
+            //   marginTop:"-88px",
+            //   marginBottom:"-88px"
+          }}
+        >
 
-    <div style={containerStyle}/>
-    <div style={containerStyleLeft}/>
-<ContainerBox
-sx={{backgroundColor:"white", width:"50%", height:1400, border:"2px solid var(--medium-green)"}}>
-<div><h1 style={{fontStyle:"italic", color:"var(--medium-green)"}} >Water Wise AI</h1></div>
-<div>
-<p style={{width: "500px",  flex: "1", paddingRight: "20px", display: "flex", alignItems: "center"}}>The Water Wise AI system deals with calculating the amount of irrigation required for date crops in the Araba region in Israel. The system will use existing models and especially the Evapotranspiration formula in order to automatically calculate the irrigation needs of Tamari Majhol in the Araba. Our system offers a comprehensive solution by integrating factors and real-time data specifically adapted to the needs of growing dates in the Arava region, such as soil characteristics, crop coefficients and local weather data. This integration ensures that the farmers will receive the most accurate and practical insights for irrigation management, while addressing the specific challenges facing date farming with great precision and efficiency.</p>
+          <div style={containerStyle} />
+          <div style={containerStyleLeft} />
+          <ContainerBox
+            sx={{ backgroundColor: "white", width: "60%", height: 640, border: "2px solid var(--medium-green)" }}>
+            <div><h1 style={{ fontStyle: "italic", color: "var(--medium-green)" }} >Water Wise AI</h1></div>
+            <Grid container spacing={4}>
+              <Grid item xs={7}>
+                The Water Wise AI system deals with calculating the amount of irrigation required for date crops in the Araba region in Israel. The system will use existing models and especially the Evapotranspiration formula in order to automatically calculate the irrigation needs of Tamari Majhol in the Araba.
+              </Grid>
+              <Grid item xs={5}></Grid>
+              <Grid item xs={5}></Grid>
+              <Grid item xs={7}>
+                Our system offers a comprehensive solution by integrating factors and real-time data specifically adapted to the needs of growing dates in the Arava region, such as soil characteristics, crop coefficients and local weather data. This integration ensures that the farmers will receive the most accurate and practical insights for irrigation management, while addressing the specific challenges facing date farming with great precision and efficiency
+              </Grid>
+              <Grid item xs={3} >
+              </Grid>
+              <Grid item xs={6} justifyContent="center" style={{ marginTop: '40px' }}>
+                "Join us today in harnessing the power of data to cultivate smarter, conserve better, and grow sustainably with Water Wise AI."
+                <CustomButton label={"Start Saving Water"} to={"/WaterCalculator"} type="button" />
+              </Grid>
+              <Grid item xs={3}>
+              </Grid>
+            </Grid>
+          </ContainerBox>
+        </Box>
 
-<image
-      style={{
-          backgroundImage: `url(${irrigationImg})`,
-        //   backgroundSize: 'cover',
-          // backgroundPosition: 'center',
-          backgroundRepeat: "no-repeat",
-          // height: '100vh',
-          // width: '100vw',
-          // display: 'flex',
-          // marginTop:"0px",
-          maxWidth: "100%",
-          height: "auto",
-          display: "flex",
-alignItems: "center"
-          // marginBottom:"88px"
-         }}
-  ></image>
-</div>
-<Box style={{width: "500px", marginLeft:"220px", marginTop:"350px"}}>"Join us today in harnessing the power of data to cultivate smarter, conserve better, and grow sustainably with Water Wise AI."</Box>
-<Box style={{width: "500px", marginLeft:"220px", marginTop:"0px"}}><CustomButton  label={"Start Saving Water"} to={"/WaterCalculator"} type="button" /></Box>
-
+      }>
 
 
-
-
-
-
-</ContainerBox>
-      </Box>
-
-    }>
-      
-      
     </PageContainer>
   );
 }
