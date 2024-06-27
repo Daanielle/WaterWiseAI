62977a81c1a6d1434b951e5d401cedc3148121eb
HadarSabati <hadarsabati@gmail.com>
2024-06-27 13:40:36 +0300

    mapping and build

--- water-wize-ai/src/pages/Home.js
+++ water-wize-ai/src/pages/Home.js
@@ -66,7 +66,7 @@ function Home() {
               <Grid item xs={7}>
                 {dict.HomeText1}
               </Grid>
-              <Grid item xs={5}></Grid>
+                <Grid item xs={5}></Grid>
               <Grid item xs={5}></Grid>
               <Grid item xs={7}>
                 {dict.HomeText2}
