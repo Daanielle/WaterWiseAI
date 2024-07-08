d44782f65fa0006afd378169b602fbe10847ca4d
LanaAbo <lana.aburya@gmail.com>
2024-07-08 18:01:26 +0300

    puu-requests

--- water-wize-ai/src/components/CustomCard.js
+++ water-wize-ai/src/components/CustomCard.js
@@ -1,50 +1,96 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { Icon } from '@mui/material';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Icon } from "@mui/material";
+import VirtecalSlider from "./water-calculator/VirtecalSlider";
 
-const CustomCard = ({ title, description, image, topIcon, bottomIcons, sx }) => {
-    return (
-        <Card sx={{ 
-            width: 170, 
-            height: '100%', 
-            display: 'flex', 
-            flexDirection: 'column', 
-            border: "2px solid var(--medium-green)", 
-            color: "var(--text-color)", 
-            //marginLeft: "10px", 
-            //marginRight: "20px",
-            borderRadius: '15px', 
-            ...sx 
-        }}>
-            {image && (
-                <CardMedia
-                    style={sx?.image}
-                    image={image}
-                    component="img"
-                    alt={title}
-                />
-            )}
+const CustomCard = ({
+  title,
+  description,
+  image,
+  topIcon,
+  bottomIcons,
+  VerticaleIcon,
+  Min,
+  Max,
+  Value,
+  Onchange,
 
-            <CardContent sx={{ flexGrow: 1, paddingBottom: 0 }}>
-                {topIcon && <Icon component={topIcon} sx={{ color: "var(--medium-green)", ...sx?.icon }} />}
-                <Typography gutterBottom component="div" fontWeight="bold" fontSize={13}>
-                    {title}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    {description}
-                </Typography>
-            </CardContent>
-            {bottomIcons && (
-                <CardActions sx={{ display: 'flex', justifyContent: 'center', mt: 'auto', paddingTop: 0 }} disableSpacing>
-                    {bottomIcons}
-                </CardActions>
-            )}
-        </Card>
-    );
-}
+  sx,
+}) => {
+  return (
+    <Card
+      sx={{
+        width: 170,
+        height: "100%",
+        display: "flex",
+        // flexDirection: 'column',
+        flexDirection: "row",
+        border: "2px solid var(--medium-green)",
+        color: "var(--text-color)",
+        borderRadius: "15px",
+        ...sx,
+      }}
+    >
+        {VerticaleIcon && (
+            <VirtecalSlider
+            iconuser={VerticaleIcon}
+            min={Min}
+            max={Max}
+            value={Value}
+            onChange={Onchange} />
+         )}
+      
+      <div style={{ display: "flex", flexDirection: "column", flexGrow: 1 }}>
+        {image && (
+          <CardMedia
+            style={sx?.image}
+            image={image}
+            component="img"
+            alt={title}
+          />
+        )}
+
+
+        <CardContent component="div" sx={{ flexGrow: 1, paddingBottom: 0 }}>
+          {topIcon && (
+            <Icon
+              component={topIcon}
+              sx={{ color: "var(--medium-green)", ...sx?.icon }}
+            />
+          )}
+          <Typography
+            gutterBottom
+            component="div"
+            fontWeight="bold"
+            fontSize={13}
+          >
+            {title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {description}
+          </Typography>
+        </CardContent>
+
+        {bottomIcons && (
+          <CardActions
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              mt: "auto",
+              paddingTop: 0,
+            }}
+            disableSpacing
+          >
+            {bottomIcons}
+          </CardActions>
+        )}
+      </div>
+    </Card>
+  );
+};
 
 export default CustomCard;
